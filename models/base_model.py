d1bf1ef8cfe422e1c2d078ac40e1e78c8b654e36
kiemaBruce <brucekiema292@gmail.com>
2024-02-10 13:50:14 +0300

    Enable **kwargs initialization of BaseModel objects

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,19 @@ import uuid
 class BaseModel:
     """Defines common attributes/methods for other classes."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes instance attributes."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """Updates updated_at with current datetime"""
