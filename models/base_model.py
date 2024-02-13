75b00ce0cab94a6c2bae34b344d9b5c5ff79f3e0
kiemaBruce <brucekiema292@gmail.com>
2024-02-13 05:17:18 +0300

    Revert previous two changes
    Check for duplicates before adding objects into __objects, and add entries for
    both objects formed from dictionaries and those formed otherwise(i.e., new
    objects)

--- models/base_model.py
+++ models/base_model.py
@@ -23,10 +23,10 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            # storage.new(self)
+        dict_key = "BaseModel.{self.id}"
+        if dict_key not in storage.all():
             storage.new(self)
-            #dict_key = "BaseModel.{self.id}"
-            #if dict_key not in storage.all():
-                #storage.new(self)
 
     def save(self):
         """Updates updated_at with current datetime"""
