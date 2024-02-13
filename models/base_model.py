17e2fd63a3f548ca7627523465913b8aee9dee97
kiemaBruce <brucekiema292@gmail.com>
2024-02-13 04:01:50 +0300

    Implement instance persistence by storing in json file

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 
 from datetime import datetime
+from models import storage
 import uuid
 
 
@@ -22,15 +23,20 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            # storage.new(self)
+        dict_key = "BaseModel.{self.id}"
+        if dict_key not in storage.all():
+            storage.new(self)
 
     def save(self):
         """Updates updated_at with current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Returns a dict with all keys/values of __dict__ of the instance."""
         full_dict = self.__dict__.copy()
-        # 		updated_at_str = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        # updated_at_str = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         full_dict["created_at"] = self.created_at.isoformat()
         full_dict["updated_at"] = self.updated_at.isoformat()
         full_dict["__class__"] = self.__class__.__name__
