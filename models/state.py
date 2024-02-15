bf8e07f9ed568851c9a7ae1f717bf31d7bb26d3f
kiemaBruce <brucekiema292@gmail.com>
2024-02-15 11:45:25 +0300

    Create State class

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Contains definition of State class."""
+
+
+class State(BaseModel):
+    """Attributes and methods of State clsss
+
+    Attributes:
+            name (str): the name of the state.
+
+    """
+
+    name = ""
