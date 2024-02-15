8c90fbcc9c769c845075ece1a68501b5b5816a65
kiemaBruce <brucekiema292@gmail.com>
2024-02-15 11:45:59 +0300

    Create Amenity class

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Contains Amenity class"""
+
+
+class Amenity(BaseModel):
+    """Attributes and methods of Amenity class.
+
+    Attributes:
+            name (str): the name of the amenity.
+    """
+
+    name = ""
