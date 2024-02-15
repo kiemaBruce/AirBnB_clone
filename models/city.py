2b83e6cfb58716ebc880d632cd7dcbc1045575d1
kiemaBruce <brucekiema292@gmail.com>
2024-02-15 11:45:45 +0300

    Create City class

---
+++ models/city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Contains definition of City class"""
+
+
+class City(BaseModel):
+    """Attributes and methods of City class
+
+    Attributes:
+            state_id (str): the state id. It is State.id
+            name (str): the name of the city.
+
+    """
+
+    state_id = ""
+    name = ""
